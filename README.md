cdd1c8fa896968a297d0fda890fc519d59965af1
蠢蠢 <727471643@qq.com>
2020-02-29 22:18:32 +0800

    init
    Signed-off-by: 蠢蠢 <727471643@qq.com>

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # ts.dll
-通过nodejs调用天使插件
+
+### 通过nodejs调用天使插件
+详情可以参考 Ts帮助文档.chm 
+
+### 参考资料:
+[aweiu/dm.dll(github)]: https://github.com/aweiu/dm.dll
+[aweiu/dm.dll(blog)]: https://aweiu.com/documents/dm.dll/
---
+++ src/index.js
@@ -0,0 +1,31 @@
+"use strict";
+const winax = require("winax");
+const child_process_1 = require("child_process");
+const path = require("path");
+const fs = require("fs");
+const _ = require("lodash");
+
+function getTSPlug() {
+    const dllExist = fs.existsSync(`${path.resolve(__dirname, 'ts.dll')}`);
+    if(!dllExist){
+        return {
+            errorCode: -1,
+            errorMsg: 'ts.dll文件不存在, 无法初始化'
+        }
+    }
+    try {
+        return new winax.Object('ts.tssoft');
+    }
+    catch (e) {
+        child_process_1.execSync(`regsvr32 ${path.resolve(__dirname, 'ts.dll')} /s`);
+        return new winax.Object('ts.tssoft');
+    }
+}
+
+const TSPlug = getTSPlug();
+
+module.exports = {
+    errorCode: 0,
+    errorMsg: 'TSPlug初始化成功',
+    ...TSPlug
+};
